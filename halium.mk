42c1e0a756e56cf1cb76d217de629a7c88ac6c1f
berbascum <berbascum@ticv.cat>
2025-12-31 00:55:19 +0100

    halium.mk: remove droidmedia service init files
    The two droidmedia service files are causing craches on some
    core services.
    
    However, it has been proven that they are not necessary
    and the droidian camera is fixed anyway.
    
    This commit removed definitively the droidmedia service init files.
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- halium.mk
+++ halium.mk
@@ -359,9 +359,3 @@ PRODUCT_PACKAGES += \
     micshm.sh \
     minimediaservice \
     minisfservice
-
-# Droidmedia init scripts
-PRODUCT_COPY_FILES += \
-    vendor/halium/droidmedia/init/minimediaservice.rc:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/init/minimediaservice.rc \
-    vendor/halium/droidmedia/init/minisfservice.rc:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/init/minisfservice.rc
-
